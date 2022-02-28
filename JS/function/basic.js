1d2118f826f573319d223f2488fcd4c43f87fa41
venkateshsekaran <venkateshsekaran18@gmail.com>
2022-02-28 17:53:20 +0530

    function

---
+++ JS/function/basic.js
@@ -0,0 +1,30 @@
+/*      model    1*/
+function add()
+{
+    console.log("addition");
+}
+add ()
+
+/*      model    2*/
+function addi()
+{
+    console.log("addition");
+}
+addi (10,20)
+
+/*      model    3*/
+function add_1(a,b)
+{
+    console.log(a+b);
+}
+add_1 (10,20)
+add_1 (20,30)
+
+/*      model    4*/
+function add_2(rahul,preethi)
+{
+    console.log(rahul);
+    console.log(preethi);
+    console.log(rahul+preethi);
+}
+add_2 (10,20)
---
+++ JS/function/basicwithreturn.js
@@ -0,0 +1,34 @@
+/*      model    1*/
+function wish()
+{
+    return "hello, Good Morning"
+}
+var message= wish()
+console.log(message);
+
+/*      model    2*/
+
+
+function add(a,b)
+{
+    return a+b
+}
+var addi= add(10,20)
+console.log(addi);
+
+/*      model    3*/
+function display(msg)
+{
+    return "hello" + msg
+}
+var results = display("gm")
+console.log(results)
+
+/*      model    3*/
+function ad(msg)
+{   
+     console.log("GN");
+    return "hello" + msg
+}
+var results = ad("gm")
+console.log(results)
