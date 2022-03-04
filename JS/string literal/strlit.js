0546128b8d9d63108971a7e54b00b202ee7c0934
venkateshsekaran <venkateshsekaran18@gmail.com>
2022-03-04 10:15:19 +0530

    emp

---
+++ JS/string literal/strlit.js
@@ -0,0 +1,6 @@
+var nam="venkatesh"
+var salary=45000
+console.log("Employee Name:" +  nam +  "and employee salary:" +  salary);
+
+
+console.log(`Employee Name: ${nam} and Employee Salary: ${salary}`);
