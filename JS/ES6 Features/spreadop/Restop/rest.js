3456172a3f613aad61f1ebd1549ca9a29f3fa9d5
venkateshsekaran <venkateshsekaran18@gmail.com>
2022-03-08 12:58:27 +0530

    restop

---
+++ JS/ES6 Features/spreadop/Restop/rest.js
@@ -0,0 +1,20 @@
+// normal one
+/*function get(a)
+{
+    console.log(a);
+}
+get(10)
+get(10,20)
+get(10,20,30)
+get(10,20,30,40)*/
+
+//Rest op to invoke remaining values
+function get(a,...b)
+{
+   /* console.log(a);*/
+    console.log(b)
+}
+get(10)
+get(10,20)
+get(10,20,30)
+get(10,20,30,40)
