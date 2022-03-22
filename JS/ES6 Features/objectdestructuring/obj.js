670367199ab41c8dff3feeb89895d6ff10ba3b35
venkateshsekaran <venkateshsekaran18@gmail.com>
2022-03-22 14:14:07 +0530

    arraydestru

--- JS/ES6 Features/objectdestructuring/obj.js
+++ JS/ES6 Features/objectdestructuring/obj.js
@@ -1,9 +1,24 @@
-let emp={id:1001,name:"venkat",gender:"male",age:25}
+const emp = {
+  id: 1001,
+  name: "venkat",
+  gender: "male",
+  age: [25, 27, 29],
+  order: function (ageindex) {
+    return this.age[ageindex];
+  },
+};
 
 //normally we print like the way
 /*console.log(emp.id);*/
 
-
 //object destructuring
-let {id}=emp
-console.log(id);
+/*let { id } = emp;
+console.log(id);*/
+
+//array destructuring
+let [a, , c] = emp.age;
+console.log(a, c);
+[c, a] = [a, c];
+console.log(a, c);
+
+console.log(emp.order(1));
