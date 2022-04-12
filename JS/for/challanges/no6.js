148b8cd0bacd5bbac2e831b4807171880ae322e7
venkateshsekaran <venkateshsekaran18@gmail.com>
2022-04-12 23:32:57 +0530

    mod

--- JS/for/challanges/no6.js
+++ JS/for/challanges/no6.js
@@ -10,8 +10,21 @@ function myFunction(a, b) {
 let output = myFunction({ x: "a", y: "b", z: "c" }, "a");
 console.log(output);*/
 
-function myFunction(a) {
+/*function myFunction(a) {
   return a.slice(0, -3);
 }
 let output2 = myFunction("abcdefg");
-console.log(output2);
+console.log(output2);*/
+
+function myFunction(a, b) {
+  let temp = "";
+  for (var i = a.length; i >= 0; ) {
+    console.log(i);
+    if (i > 2) temp = b + a.substring(i - 3, i) + temp;
+    else temp = a.substring(i - 3, i) + temp;
+    i -= 3;
+  }
+  return temp;
+}
+let output = myFunction("1234567", ".");
+console.log(output);
