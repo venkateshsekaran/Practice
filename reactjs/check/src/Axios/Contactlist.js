0a755450a85aaa5c4357e2607899a045f4865cc6
venkateshsekaran <venkateshsekaran18@gmail.com>
2022-04-15 16:10:42 +0530

    fun

--- reactjs/check/src/Axios/Contactlist.js
+++ reactjs/check/src/Axios/Contactlist.js
@@ -1,45 +1,45 @@
 import React from "react";
-class Contactlist extends React.Component {
-  sethandler = (contact) => {
-    this.props.selecteddata(contact);
+function Contactlist(props) {
+  let sethandler = (contact) => {
+    props.selecteddata(contact);
   };
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <table className="table table-hover">
-              <thead className="bg-secondary text-white">
-                <tr>
-                  <th>Id</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Gender</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.props.contacts.map((contact) => {
-                  return (
-                    <tr
-                      key={contact.login.uuid}
-                      onMouseOver={this.sethandler.bind(this, contact)}
-                    >
-                      <td>{contact.login.uuid.slice(-4)}</td>
-                      <td>{contact.name.first.toUpperCase()}</td>
-                      <td>{contact.email}</td>
-                      <td>
-                        {contact.gender.slice(0, 1).toUpperCase() +
-                          contact.gender.slice(1)}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <table className="table table-hover">
+            <thead className="bg-secondary text-white">
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Gender</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.contacts.map((contact) => {
+                return (
+                  <tr
+                    key={contact.login.uuid}
+                    onMouseOver={sethandler.bind(this, contact)}
+                  >
+                    <td>{contact.login.uuid.slice(-4)}</td>
+                    <td>{contact.name.first.toUpperCase()}</td>
+                    <td>{contact.email}</td>
+                    <td>
+                      {contact.gender.slice(0, 1).toUpperCase() +
+                        contact.gender.slice(1)}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default Contactlist;
--- reactjs/check/src/Axios/Home1.js
+++ reactjs/check/src/Axios/Home1.js
@@ -1,11 +1,10 @@
 import React from "react";
-class Home1 extends React.Component {
-  render() {
-    return (
-      <div>
-        <h3>Home Page Component</h3>
-      </div>
-    );
-  }
-}
+let Home1 = () => {
+  return (
+    <div>
+      <h3>Home Page Component</h3>
+    </div>
+  );
+};
+
 export default Home1;
