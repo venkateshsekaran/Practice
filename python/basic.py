64d88a22ef326659ee381369a9e5c22e25ac4c88
venkateshsekaran <venkateshsekaran18@gmail.com>
2022-04-19 15:50:19 +0530

    python

---
+++ python/basic.py
@@ -0,0 +1,21 @@
+#work with function
+def add():
+    return "HellO"
+
+print(add())
+
+# to find the datatype
+price = 4.5
+c = 100+200j
+p_Name="iphone 12"
+is_Avail = True
+
+print(type(c))
+print(type(price))
+print(type(p_Name))
+print(type(is_Avail))
+
+# to know what is keyword and its length
+import keyword
+print((keyword.kwlist))
+print(len(keyword.kwlist))
